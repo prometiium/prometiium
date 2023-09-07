7e736d6f8a4ca8553528981061399f5938d4c294
prometiium <136849128+prometiium@users.noreply.github.com>
2023-09-07 13:11:23 +0300

    Add files via upload

---
+++ set-liste.py
@@ -0,0 +1,21 @@
+
+liste=['a','b','c','d']
+liste=liste+['f','ş','u','t','ı']
+liste.append('ğ')
+liste
+liste2=['1','5', '2', '6', '0', '3', '4', '3']
+liste2.append('7')
+liste2.sort()
+liste2=['1','5', '2', '6', '0', '3', '4', '3']
+liste2.append('7')
+a=input('')
+liste2.append(a)
+set1=set(liste)
+liste2.sort()
+set2=set(liste2)
+
+
+print('\n\n\n')
+print(liste)
+print('--------------------------------------------------')
+print(liste2)
