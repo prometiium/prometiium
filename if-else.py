e9dcbdff6de8249fa3eeb34ac9c69c6c41251b02
prometiium <136849128+prometiium@users.noreply.github.com>
2023-09-07 13:10:43 +0300

    Add files via upload

---
+++ if-else.py
@@ -0,0 +1,13 @@
+while 'True':
+    a=input('a?\n')
+    list=['q','w','e','r','t','t','y','u','ı','o','p','ğ','ü','a','s','d','f','g','h','j','k','l','ş','i','z','x','c','v','b','n','m','ö','ç']
+
+    if a in list:
+        print('break')
+        
+    elif(int(a)<10):
+        print('True')
+    else:
+        print('False')
+
+        
